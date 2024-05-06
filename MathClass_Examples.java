db2721c12006e043f99542af8319da393eff461f
SreeKarri <jyokarri@gmail.com>
2024-05-06 11:11:37 -0400

    MathClss

---
+++ MathClass_Examples.java
@@ -0,0 +1,19 @@
+package mathClass_April_2nd_24;
+
+public class MathClass_Examples {
+
+	public static void main(String[] args) {
+		double pivalue =Math.PI;
+		System.out.println("Pivalue"+pivalue);
+		System.out.println(Math.addExact(100, 300));
+		System.out.println(Math.subtractExact(900, 80));
+		System.out.println(Math.max(57.2, 88.8)); //gives which number is higher
+		System.out.println(Math.min(100.2, 88.8));
+		System.out.println(Math.max(66.9, 999.6));
+		System.out.println(Math.min(8765,543209)); //gives minimum value
+		System.out.println(Math.random());
+		System.out.println(Math.sqrt(6754.9));
+
+	}
+
+}
